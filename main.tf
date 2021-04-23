eca7f89bde601a8f223e5b5345e059da77cee6fa
Kavya Soni <79825106+kavyasonii@users.noreply.github.com>
2021-04-23 15:30:06 +0530

    Update main.tf

--- main.tf
+++ main.tf
@@ -1,3 +1,6 @@
+
+# We strongly recommend using the required_providers block to set the
+# Azure Provider source and version being used
 terraform {
   required_providers {
     azurerm = {
@@ -12,6 +15,8 @@ provider "azurerm" {
   features {}
 
   subscription_id = "60d56aba-6483-43bc-8730-61cbb29ec534"
+  client_id       = "69902f09-e534-444b-a852-5406db483372"
+  client_secret   = var.client_secret
   tenant_id       = "0c14f0b2-ceab-47a0-9df5-e8c0d727df3e"
 }
 # Create a resource group
